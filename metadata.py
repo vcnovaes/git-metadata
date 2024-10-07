b3dedbab9a97975eb7e08c60753508e421be341f
vini <vcn2@cin.ufpe.br>
2024-10-07 00:01:02 -0300

    add on lint schema

--- metadata.py
+++ metadata.py
@@ -1,19 +1,17 @@
+"""Module for run on terminal"""
+
 import subprocess
-import csv
-from collections import defaultdict
 
 
 class FileNode:
+    """Data reprentation of a file"""
+
     def __init__(self, label: str, count: int = 0, children=None) -> None:
-        if children == None:
+        if children is None:
             self.children = dict()
 
         self.label = label
         self.count = count
-        pass
-
-    def increase_modification_count(self):
-        self.count += 1
 
     def __str__(self) -> str:
         return f"{self.label}"
@@ -23,11 +21,14 @@ class FileNode:
 
 
 class FileTree:
+    """File Tree"""
+
     def __init__(self, root_node_name: str) -> None:
         self.root = FileNode(root_node_name)
 
     def print(self, node: FileNode = None, counter=0):
-        if node == None:
+        """Print the file tree"""
+        if node is None:
             node = self.root
         if "." in node.label:
             print(f"{node.label}({node.count})", end="\n")
@@ -54,6 +55,7 @@ class FileTree:
         return path_nodes
 
     def add_file(self, filename: str):
+        """ "Add the file on the filetree"""
         # adicionar na arvore
         path_nodes = self.__find_or_create_path(filename)
         # somar count nos anteriores
@@ -61,22 +63,36 @@ class FileTree:
             node.count += 1
 
     def register_modification(self, filename: str):
+        """register the modification of an existing file"""
         self.add_file(filename)
 
     def delete_file(self, filename: str):
-        pass
-
-    def rename_file(self, old_filename: str, new_filename: str):
-        pass
+        """Delete file from the tree"""
+        path_nodes = self.__find_or_create_path(filename)
+        file_node = path_nodes[-1]
+        parents = path_nodes[:-1]
+        to_remove = [file_node]
+        for parent in reversed(parents):
+            if len(parent.children) == 1:
+                to_remove.append(parent)
+                file_node = parent
+            else:
+                del parent.children[file_node.label]
+                break
+        for file in to_remove:
+            del file
 
 
 class GitChange:
+    """Class for store git changes"""
+
     def __init__(self, modification_type: str, filepath: str) -> None:
         self.type = modification_type
         self.filepath = filepath
 
     @staticmethod
     def from_string(raw_modification_log: str):
+        """Method that desserialize the gitlog change"""
         try:
             change_type, filename = raw_modification_log.split("\t", 1)
             return GitChange(change_type, filename)
@@ -84,13 +100,15 @@ class GitChange:
             print(f"ValueError: {error} \t {raw_modification_log} was not processed")
 
     def __str__(self) -> str:
-        return (self.type, self.filepath)
+        return f"{self.type, self.filepath}"
 
 
 def __raw_git_data():
     return (
         subprocess.run(
-            ["git", "log", "--name-status", "--pretty=format:"], stdout=subprocess.PIPE
+            ["git", "log", "--name-status", "--pretty=format:"],
+            stdout=subprocess.PIPE,
+            check=False,
         )
         .stdout.decode()
         .splitlines()
@@ -98,6 +116,7 @@ def __raw_git_data():
 
 
 def git_changes(root_dir: str = "src"):
+    """Method that get git changes"""
     node_changes = [
         GitChange.from_string(raw_node)
         for raw_node in __raw_git_data()
@@ -107,8 +126,9 @@ def git_changes(root_dir: str = "src"):
 
 
 def main():
+    """Main application function"""
     root_dir = "src"
-    change_list = git_changes(root_dir)
+    change_list = reversed(git_changes(root_dir))
     tree = FileTree(root_dir)
     for change in change_list:
         if change.type == "A":
